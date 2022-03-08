b1e121ec597ba9b5cb5e98ece39977f3f38f2fae
BZhang787 <84584425+BZhang787@users.noreply.github.com>
2022-03-08 17:35:56 -0500

    Added time change buttons

--- Pomodoro.java
+++ Pomodoro.java
@@ -3,36 +3,34 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.TrayIcon.MessageType;
 public class Pomodoro implements ActionListener{
-	JFrame frame = new JFrame();
+		JFrame frame = new JFrame();
 	JButton startButton = new JButton("START");
 	JButton resetButton = new JButton("RESET");
+	JButton timeIncrease = new JButton("+1:00");
+	JButton timeDecrease = new JButton("-1:00");
 	JLabel timeLabel = new JLabel();
 	JLabel task = new JLabel();
 	int pomodoros = 0;
 	int elapsedTime = 1500000;
 	int seconds = 0;
-	int minutes = 25;
-	int hours = 0;
+	int defaultMinutes = 25;
+	int minutes;
 	boolean started = false;
 	String seconds_string = String.format("%02d", seconds);
-	String minutes_string = String.format("%02d", minutes);
-	String hours_string = String.format("%02d", minutes);
+	String minutes_string = String.format("%02d", defaultMinutes);
 	
 	Timer timer = new Timer(1000, new ActionListener() {
 		
 		public void actionPerformed(ActionEvent e) {
 			
 			elapsedTime -= 1000;
-			minutes = (elapsedTime/60000) % 60;
+			minutes = (elapsedTime/60000);
 			seconds = (elapsedTime/1000) % 60;
-			seconds_string = String.format("%02d", seconds);
-			minutes_string = String.format("%02d", minutes);
-			timeLabel.setText(minutes_string + ":" + seconds_string);
-			if(elapsedTime == 0) {
+			timeLabel.setText(formatTime(seconds, minutes));
+			if(elapsedTime <= 0) {
 				try {
 					pomodoroReset();
 				} catch (AWTException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
@@ -43,33 +41,43 @@ public class Pomodoro implements ActionListener{
 	Pomodoro(){
 		
 		task.setText("STUDY TIME");
-		task.setBounds(100, 50, 200, 50);
-		task.setFont(new Font("Verdana", Font.PLAIN, 20));
-		task.setBorder(BorderFactory.createBevelBorder(1));
+		task.setBounds(0, 10, 420, 50);
+		task.setFont(new Font("Verdana", Font.PLAIN, 25));
 		task.setOpaque(true);
 		task.setHorizontalAlignment(JTextField.CENTER);
 		
 		timeLabel.setText(minutes_string + ":" + seconds_string);
-		timeLabel.setBounds(100, 100, 200, 100);
+		timeLabel.setBounds(0, 60, 420, 150);
 		timeLabel.setFont(new Font("Verdana", Font.PLAIN, 35));
-		timeLabel.setBorder(BorderFactory.createBevelBorder(1));
 		timeLabel.setOpaque(true);
 		timeLabel.setHorizontalAlignment(JTextField.CENTER);
 		
-		startButton.setBounds(100, 200, 100, 50);
-		startButton.setFont(new Font("Verdana", Font.PLAIN, 20));
+		startButton.setBounds(0, 250, 105, 50);
+		startButton.setFont(new Font("Verdana", Font.PLAIN, 15));
 		startButton.setFocusable(false);
 		startButton.addActionListener(this);
 		
-		resetButton.setBounds(200, 200, 100, 50);
-		resetButton.setFont(new Font("Verdana", Font.PLAIN, 20));
+		resetButton.setBounds(105, 250, 105, 50);
+		resetButton.setFont(new Font("Verdana", Font.PLAIN, 15));
 		resetButton.setFocusable(false);
 		resetButton.addActionListener(this);
+		
+		timeIncrease.setBounds(210, 250, 105, 50);
+		timeIncrease.setFont(new Font("Verdana", Font.PLAIN, 15));
+		timeIncrease.setFocusable(false);
+		timeIncrease.addActionListener(this);
+		
+		timeDecrease.setBounds(315, 250, 105, 50);
+		timeDecrease.setFont(new Font("Verdana", Font.PLAIN, 15));
+		timeDecrease.setFocusable(false);
+		timeDecrease.addActionListener(this);
 		
 		frame.add(task);
 		frame.add(timeLabel);
 		frame.add(startButton);
 		frame.add(resetButton);
+		frame.add(timeIncrease);
+		frame.add(timeDecrease);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(420, 420);
@@ -97,6 +105,26 @@ public class Pomodoro implements ActionListener{
 			startButton.setText("START");
 			reset();
 		}
+		if(e.getSource() == timeIncrease && started == false) {
+			elapsedTime += 60000;
+			this.defaultMinutes++;
+			minutes = (elapsedTime/60000);
+			seconds = (elapsedTime/1000) % 60;
+			timeLabel.setText(formatTime(seconds, minutes));
+		}
+		if(e.getSource() == timeDecrease && started == false) {
+			if(elapsedTime <= 60000) {
+				elapsedTime = 0;
+				this.defaultMinutes = 0;
+			}else {
+				elapsedTime -= 60000;
+				this.defaultMinutes--;
+			}
+			minutes = (elapsedTime/60000);
+			seconds = (elapsedTime/1000) % 60;
+			timeLabel.setText(formatTime(seconds, minutes));
+			
+		}
 	}
 	
 	void start() {
@@ -110,12 +138,10 @@ public class Pomodoro implements ActionListener{
 	void reset() { 
 		timer.stop();
 		pomodoros = 0;
-		elapsedTime = 1500000;
+		elapsedTime = defaultMinutes * 60000;
 		seconds = 0;
-		minutes = 25;
-		seconds_string = String.format("%02d", seconds);
-		minutes_string = String.format("%02d", minutes);
-		timeLabel.setText(minutes_string + ":" + seconds_string);
+		minutes = defaultMinutes;
+		timeLabel.setText(formatTime(seconds, minutes));
 		task.setText("STUDY TIME");
 	}
 	void pomodoroReset() throws AWTException {
@@ -125,32 +151,32 @@ public class Pomodoro implements ActionListener{
 				seconds = 0;
 				minutes = 15;
 				pomodoros = -1;
-				seconds_string = String.format("%02d", seconds);
-				minutes_string = String.format("%02d", minutes);
-				timeLabel.setText(minutes_string + ":" + seconds_string);
+				timeLabel.setText(formatTime(seconds, minutes));
 				task.setText("BREAK TIME");
 				this.infoBox("It's time to take a break", "15 minutes break");
 			}
 			else if(pomodoros % 2 == 0) {
-				elapsedTime = 1500000;
+				elapsedTime = 60000*defaultMinutes;
 				seconds = 0;
-				minutes = 25;
-				seconds_string = String.format("%02d", seconds);
-				minutes_string = String.format("%02d", minutes);
-				timeLabel.setText(minutes_string + ":" + seconds_string);
+				minutes = defaultMinutes;
+				timeLabel.setText(formatTime(seconds, minutes));
 				task.setText("STUDY TIME");
-				this.infoBox("It's time to study again", "25 minutes studying");
+				this.infoBox("It's time to study again", minutes + " minutes studying");
 			}
 			else {
 				elapsedTime = 300000;
 				seconds = 0;
 				minutes = 5;
-				seconds_string = String.format("%02d", seconds);
-				minutes_string = String.format("%02d", minutes);
-				timeLabel.setText(minutes_string + ":" + seconds_string);
+				timeLabel.setText(formatTime(seconds, minutes));
 				task.setText("BREAK TIME");
 				this.infoBox("It's time to take a break", "5 minutes break");
 			}
+	}
+	
+	String formatTime(int seconds, int minutes) {
+		String seconds_string = String.format("%02d", seconds);
+		String minutes_string = String.format("%02d", minutes);
+		return (minutes_string + ":" + seconds_string);
 	}
 
 	void infoBox(String infoMessage, String titleBar) throws AWTException {
@@ -158,7 +184,7 @@ public class Pomodoro implements ActionListener{
 		Image image = Toolkit.getDefaultToolkit().createImage("PomodoroPic.jpg");
 		TrayIcon trayIcon = new TrayIcon(image, "Timer Up");
 		trayIcon.setImageAutoSize(true);
-		trayIcon.setToolTip("System tray icon demo");
+		trayIcon.setToolTip("Pomodoro Timer");
 		tray.add(trayIcon);
 		
 		trayIcon.displayMessage(infoMessage, titleBar, MessageType.INFO);
